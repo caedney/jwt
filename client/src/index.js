26043c79a44df2e49e9a2e727a8ff984a2e3db5b
Craig Edney <caedney@btinternet.com>
2022-12-16 09:48:58 +0000

    refactor: Add context provider to context file

--- client/src/index.js
+++ client/src/index.js
@@ -2,49 +2,19 @@ import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Dashboard from './components/Dashboard';
 import Signin from './components/Signin';
 import Register from './components/Register';
 
-import authenticationApi from './api/authentication';
-import AuthenticationContext from './contexts/AuthenticationContext';
-import useLocalStorage from './utils/useLocalStoarge';
+import { AuthenticationProvider } from './contexts/AuthenticationContext';
 import theme from './themes';
 
 function App() {
-  const [authenticated, setAuthenticated] = React.useState(false);
-  const [token, setToken] = useLocalStorage('jwtToken');
-
-  const authentication = {
-    authenticated,
-    setAuthenticated,
-    token,
-    setToken,
-  };
-
-  React.useEffect(() => {
-    async function isAuthenticated() {
-      try {
-        const response = await authenticationApi.get('/verified', {
-          headers: { token },
-        });
-
-        setAuthenticated(response.data.verified);
-      } catch (error) {
-        toast(error.response.data, {
-          type: 'error',
-        });
-      }
-    }
-
-    isAuthenticated();
-  }, [token]);
-
   return (
-    <AuthenticationContext.Provider value={authentication}>
+    <AuthenticationProvider>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Router>
@@ -56,7 +26,7 @@ function App() {
         </Router>
         <ToastContainer />
       </ThemeProvider>
-    </AuthenticationContext.Provider>
+    </AuthenticationProvider>
   );
 }
 
